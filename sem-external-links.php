dbff0d45e70235dec1f9c334bf2dc9111c69d69e
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-23 17:17:47 +0000

    add plugin info text domains
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-external-links/trunk@1461 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-external-links.php
+++ sem-external-links.php
@@ -3,9 +3,11 @@
 Plugin Name: External Links
 Plugin URI: http://www.semiologic.com/software/external-links/
 Description: Adds a class=&quot;external&quot; to all outbound links, with various effects that are configurable under Options / External Links. Use &lt;a class=&quot;no_icon&quot; ...&gt; to disable the icon on individual links.
-Version: 3.0.5 RC
+Version: 3.1 alpha
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
+Text Domain: sem-external-links-info
+Domain Path: /lang
 */
 
 /*
