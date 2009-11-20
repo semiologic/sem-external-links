35e9ec2331d0326260740f7eb5be795c49592461
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-11-20 15:37:04 +0000

    more local url handling
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-external-links/trunk@3380 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-external-links.php
+++ sem-external-links.php
@@ -86,9 +86,10 @@ class external_links {
 	 * @return bool $is_local_url
 	 **/
 
-	function is_local_url($url) {
 		if ( in_array(substr($url, 0, 1), array('?', '#')) || strpos($url, '://') === false )
 			return true;
+		elseif ( preg_match("~/go(/|\.)~i", $url) )
+			return false;
 		
 		static $site_domain;
 		
